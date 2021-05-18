361458c080c5ffaa25c13bbb93006fe95435cac1
Mathias Payen <mathias.payen@gmail.com>
2021-05-18 21:28:18 +0200

    test

--- app.js
+++ app.js
@@ -84,10 +84,14 @@ app.use((err, req, res, next) => {
 });
 
 
+
+
 // app.use(express.static(path.join(__dirname, "..", "client-build")));
-// app.use(express.static("../client-build"));
+app.use(express.static(path.join(__dirname, "../client-build")));
 
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client-build/index.html"))
+})
 // app.use(express.static(path.join(__dirname, "../client-build")));
 
 // app.get("*", (req, res) => {
@@ -97,15 +101,15 @@ app.use((err, req, res, next) => {
 
 
 
-// Server static assets if in production
-if(process.env.NODE_ENV === 'production') {
-    // set static folder
-    app.use(express.static('client/client-build'))
+// // Server static assets if in production
+// if(process.env.NODE_ENV === 'production') {
+//     // set static folder
+//     app.use(express.static('../client-build'))
 
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'client-build', 'index.html'));
-    } )
-}
+//     app.get('*', (req,res) => {
+//         res.sendFile(path.resolve(__dirname, '..', 'client-build', 'index.html'));
+//     } )
+// }
 
 const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
