f867986595c424d77082c95791de2ab312a0eb7e
Mathias Payen <mathias.payen@gmail.com>
2021-05-19 13:35:45 +0200

    test

--- app.js
+++ app.js
@@ -92,6 +92,13 @@ app.use(express.static(path.join(__dirname, "../client-build")));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "../client-build/index.html"))
 })
+
+
+const PORT = process.env.PORT || 3030;
+app.listen(PORT, () => {
+    console.log('listening on' + PORT);
+});
+
 // app.use(express.static(path.join(__dirname, "../client-build")));
 
 // app.get("*", (req, res) => {
@@ -110,11 +117,6 @@ app.get('*', (req, res) => {
 //         res.sendFile(path.resolve(__dirname, '..', 'client-build', 'index.html'));
 //     } )
 // }
-
-const PORT = process.env.PORT || 3030;
-app.listen(PORT, () => {
-    console.log('listening on' + PORT);
-});
 
 
 
