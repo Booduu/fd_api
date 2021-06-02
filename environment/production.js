e47f4e1465871c6ad298f2d55f1938d1b75a448c
Mathias Payen <mathias.payen@gmail.com>
2021-06-02 13:28:29 +0200

    test

--- environment/production.js
+++ environment/production.js
@@ -1,6 +1,9 @@
 const path = require('path');
 module.exports = {
     dbUrl: 'mongodb://localhost:27017/fulldub',
-    cert: path.join(__dirname, ''),
-    key: path.join(__dirname, ''),
+    cert: '/etc/letsencrypt/live/www.boodu.fr/fullchain.pem',
+    key: '/etc/letsencrypt/live/www.boodu.fr/privkey.pem',
 }
+
+
+// certbot certonly -d www.boodu.fr -d boodu.fr
